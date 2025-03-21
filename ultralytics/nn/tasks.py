f2a69978b4029757efda7a1bc2a54a67ea33fb40
INVINCIBLE2297 <108004939+INVINCIBLE2297@users.noreply.github.com>
2025-03-21 15:03:01 +0800

    Update tasks.py
    Signed-off-by: INVINCIBLE2297 <108004939+INVINCIBLE2297@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -64,6 +64,7 @@ from ultralytics.nn.modules import (
     WorldDetect,
     v10Detect,
     CoordAtt,
+    SKAttention,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1196,6 +1197,11 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 n = 1
         elif m is ResNetLayer:
             c2 = args[1] if args[3] else args[1] * 4
+        elif m is SKAttention:
+            c1, c2 = ch[f], args[0]
+            if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
+                c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1, c2, *args[1:]]
         elif m is torch.nn.BatchNorm2d:
             args = [ch[f]]
         elif m is Concat:
