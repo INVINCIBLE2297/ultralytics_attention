303c34263de4a7fe6fffd10ce85502f3abe617eb
INVINCIBLE2297 <108004939+INVINCIBLE2297@users.noreply.github.com>
2025-03-21 17:36:41 +0800

    Update tasks.py
    Signed-off-by: INVINCIBLE2297 <108004939+INVINCIBLE2297@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -65,6 +65,7 @@ from ultralytics.nn.modules import (
     v10Detect,
     CoordAtt,
     SKAttention,
+    CBAM,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1226,6 +1227,11 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args = [*args[1:]]
         elif m in {CoordAtt}:
             args = [ch[f], *args]
+        elif m is CBAM:
+            c1, c2 = ch[f], args[0]
+            if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
+                c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1,  *args[1:]]
         else:
             c2 = ch[f]
 
