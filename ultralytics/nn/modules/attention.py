e22c4631b30f24a72bbb76b7579acb279dc17c52
INVINCIBLE2297 <108004939+INVINCIBLE2297@users.noreply.github.com>
2025-03-21 14:46:29 +0800

    Update attention.py
    Signed-off-by: INVINCIBLE2297 <108004939+INVINCIBLE2297@users.noreply.github.com>

--- ultralytics/nn/modules/attention.py
+++ ultralytics/nn/modules/attention.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from collections import OrderedDict
  
  __all__ = (
    "CoordAtt",
+   "SKAttention",
  )
 
 class h_sigmoid(nn.Module):
@@ -60,3 +62,53 @@ class CoordAtt(nn.Module):
         out = identity * a_w * a_h
  
         return out
+
+class SKAttention(nn.Module):
+    def __init__(self, channel=512, kernels=[1,3,5,7],reduction=16, group=1, L=32):
+        super().__init__()
+        self.d = max(L, channel//reduction)
+        self.convs = nn.ModuleList([])
+        for k in kernels:
+            self.convs.append(
+                nn.Sequential(OrderedDict([
+                    ('conv', nn.Conv2d(channel, channel, kernel_size=k, padding=k//2, groups=group)),
+                    ('bn', nn.BatchNorm2d(channel)),
+                    ('relu', nn.ReLU())
+                ]))
+            )
+        self.fc = nn.Linear(channel, self.d)
+        self.fcs = nn.ModuleList([])
+        for i in range(len(kernels)):
+            self.fcs.append(nn.Linear(self.d, channel))
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, x):
+        bs, c, _, _ = x.size()
+        conv_outs = []
+        # split
+        for conv in self.convs:
+            conv_outs.append(conv(x))
+        feats = torch.stack(conv_outs, 0)  # k,bs,channel,h,w
+
+        # fuse
+        U = sum(conv_outs)  # bs,c,h,w
+
+        # reduction channel
+        S = U.mean(-1).mean(-1)  # bs,c
+        Z=self.fc(S)  # bs,d
+
+        # calculate attention weight
+        weights = []
+        for fc in self.fcs:
+            weight = fc(Z)
+            weights.append(weight.view(bs, c, 1, 1))  # bs,channel
+        attention_weights=torch.stack(weights, 0)  # k,bs,channel,1,1
+        attention_weights=self.softmax(attention_weights)  # k,bs,channel,1,1
+
+        # fuse
+        V=(attention_weights*feats).sum(0)
+        return V
+
+
+    
+
